265ebca92645d71eb768df52faf6fb47df279ecb
andres.reyes <andresm.reyes@oncredit.com.co>
2024-12-12 21:17:11 -0500

    bueno

--- prueba.py
+++ prueba.py
@@ -22,7 +22,7 @@ def main():
     print("1. Add")
     print("2. Subtract")
     print("3. Multiply")
-    print("4. Divide")zaasdas
+    print("4. Divide")
 
     choice = input("Enter choice (1/2/3/4): ")
 
